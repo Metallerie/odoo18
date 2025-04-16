ef4261cb13a7330b24e228bcaa63e9ce6c5edc85
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-04-16 11:21:45 +0200

    Update __manifest__.py

--- myaddons/website_link_checker/__manifest__.py
+++ myaddons/website_link_checker/__manifest__.py
@@ -8,8 +8,11 @@
     "category": "Website",
     "summary": "Scan des liens de sitemap et envoi d'un rapport en cas d'erreur",
     "data": [
-        "views/res_config_settings_views.xml",
+        "views/website_views.xml",
         "views/website_page_views.xml",
+        "views/blog_post_views.xml",
+        "views/product_template_views.xml",
+        "views/forum_post_views.xml",
         "data/ir_cron.xml",
     ],
     "installable": True,
