cbcc494649e563b83cb7bd71835038118318857e
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-04-16 18:27:23 +0200

    Update __manifest__.py

--- myaddons/website_link_checker/__manifest__.py
+++ myaddons/website_link_checker/__manifest__.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 # __manifest__.py
 {
-    "name": "Website Link Checker",
+    'name': 'Website Link Checker',
     'version': '18.0.1.0.0',
-    "depends": ["website", "mail"],
-    'website': "https://www.metallerie.xyz",
-    "author": "Franck & Gpt",
-    "category": "Website",
-    "summary": "Scan des liens de sitemap et envoi d'un rapport en cas d'erreur",
-    "data": [
-        "views/website_page_views.xml",
-        "data/ir_cron.xml"
+    'depends': ['website', 'mail'],
+    'website': 'https://www.metallerie.xyz',
+    'author': 'Franck & Gpt',
+    'category': 'Website',
+    'summary': "Scan des liens de sitemap et envoi d'un rapport en cas d'erreur",
+    'description': """envoi d'un rapport en cas d'erreur""",
+    'data': [
+        'views/website_page_views.xml',
+        'data/ir_cron.xml'
        
     ],
-    "installable": True,
-    "application": False
+    'installable': True,
+    'license': 'LGPL-3',
+    'application': False
     
 }
