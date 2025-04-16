6110a62ccf9256aedf34c645fbbe1106c90dc5e7
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-04-16 17:18:24 +0200

    Update __manifest__.py

--- myaddons/website_link_checker/__manifest__.py
+++ myaddons/website_link_checker/__manifest__.py
@@ -4,13 +4,15 @@
     "name": "Website Link Checker",
     "version": "1.0",
     "depends": ["website", "mail"],
-    "author": "Franck",
+    "author": "Franck & Gpt",
     "category": "Website",
     "summary": "Scan des liens de sitemap et envoi d'un rapport en cas d'erreur",
     "data": [
         "views/website_page_views.xml",
         "data/ir_cron.xml"
+       
     ],
     "installable": True,
     "application": False
+    
 }
