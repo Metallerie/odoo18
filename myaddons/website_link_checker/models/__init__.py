1243ed85ad974507548e373f6e4b8d5f7396d15c
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-04-16 11:14:08 +0200

    Update __init__.py

--- myaddons/website_link_checker/models/__init__.py
+++ myaddons/website_link_checker/models/__init__.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
-
-from . import res_config_settings
+from . import website
 from . import website_page
+from . import seo_mixin
+from . import blog_post
+from . import product_template
+from . import forum_post
+
 
