f0b9abafdc3b110902118c50a704bc66667f3af7
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-04-16 18:43:19 +0200

    Update __manifest__.py

--- myaddons/website_link_checker/__manifest__.py
+++ myaddons/website_link_checker/__manifest__.py
@@ -9,6 +9,9 @@
     'category': 'Website',
     'summary': "Scan des liens de sitemap et envoi d'un rapport en cas d'erreur",
     'description': """envoi d'un rapport en cas d'erreur""",
+    'installable': True,
+    'license': 'LGPL-3',
+    'application': False
     'data': [
         'views/website_page_views.xml',
         'data/ir_cron.xml'
