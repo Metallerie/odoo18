294cbfe50afcb4fa01c1cf0ab52d5e69bedcafdc
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-04-16 10:54:41 +0200

    Update __manifest__.py

--- myaddons/website_link_checker/__manifest__.py
+++ myaddons/website_link_checker/__manifest__.py
@@ -1,34 +1,17 @@
 # -*- coding: utf-8 -*-
+# __manifest__.py
 {
-    'name': "website_link_checker",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
-    'description': """
-Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+    "name": "Website Link Checker",
+    "version": "1.0",
+    "depends": ["website", "mail"],
+    "author": "Franck",
+    "category": "Website",
+    "summary": "Scan des liens de sitemap et envoi d'un rapport en cas d'erreur",
+    "data": [
+        "views/res_config_settings_views.xml",
+        "views/website_page_views.xml",
+        "data/ir_cron.xml",
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    "installable": True,
+    "application": False,
 }
-
