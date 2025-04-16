b695ef74cb3c792f9f555c82c975b0dd062cc8a5
Franck Bardina <franck@metallerie.xyz>
2025-04-16 19:53:50 +0000

    module mindee

--- myaddons/metallerie_sync_1618/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import models
-from . import sync_manager
-from . import sync_company
-from . import sync_products
-from . import sync_partners
-from . import sync_sales
-from . import sync_field_status
