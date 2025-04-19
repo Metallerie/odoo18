cb639a65a3f38bbeb63765ff2bdfb0d806541073
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-04-19 10:51:26 +0200

    Create __manifest__.py

---
+++ myaddons/robots_sitemap_https/__manifest__.py
@@ -0,0 +1,11 @@
+# fichier __manifest__.py
+# ------------------------
+{
+    'name': 'Robots and Sitemap HTTPS Fix',
+    'version': '1.0',
+    'category': 'Website',
+    'summary': 'Force HTTPS for robots.txt and sitemap.xml',
+    'depends': ['website'],
+    'installable': True,
+    'auto_install': False,
+}
