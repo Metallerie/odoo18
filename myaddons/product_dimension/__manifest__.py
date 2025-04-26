3993a1eddb7fba806f977bea70391deabd6006a5
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-04-26 07:31:39 +0200

    Update __manifest__.py

--- myaddons/product_dimension/__manifest__.py
+++ myaddons/product_dimension/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2018 brain-tec AG (http://www.braintec-group.com)
+par franck et Cgpt# Copyright 2018 brain-tec AG (http://www.braintec-group.com)
 # Copyright 2015-2016 Camptocamp SA
 # Copyright 2015 ADHOC SA  (http://www.adhoc.com.ar)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
