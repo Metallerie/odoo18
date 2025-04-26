cf4787bdf39668e9092acbd403929e3ff7e98497
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-04-26 07:33:17 +0200

    Update __manifest__.py

--- myaddons/product_dimension/__manifest__.py
+++ myaddons/product_dimension/__manifest__.py
@@ -1,4 +1,4 @@
-par franck et Cgpt# Copyright 2018 brain-tec AG (http://www.braintec-group.com)
+# Copyright 2018 brain-tec AG (http://www.braintec-group.com)
 # Copyright 2015-2016 Camptocamp SA
 # Copyright 2015 ADHOC SA  (http://www.adhoc.com.ar)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -6,10 +6,10 @@ par franck et Cgpt# Copyright 2018 brain-tec AG (http://www.braintec-group.com)
     "name": "Product Dimension",
     "version": "18.0.1.0.0",
     "category": "Product",
-    "author": "brain-tec AG, ADHOC SA, Camptocamp SA, "
+    "author": "Modifié par franck et Gpt "
     "Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "website": "https://github.com/OCA/product-attribute",
+    "website": "https://www.metallerie.xyz",
     "depends": ["product"],
     "data": ["views/product_view.xml"],
     "installable": True,
