5bb5277eaa306f5a389152079914a987fea2838c
Franck Bardina <franck@metallerie.xyz>
2025-04-26 19:45:59 +0000

    Rename module product_dimension to product_metal and prepare new structure for metal at meter management

---
+++ myaddons/product_metal/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2018 brain-tec AG (http://www.braintec-group.com)
+# Copyright 2015-2016 Camptocamp SA
+# Copyright 2015 ADHOC SA  (http://www.adhoc.com.ar)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Product Dimension",
+    "version": "18.0.1.0.0",
+    "category": "Product",
+    "author": "Modifié par franck et Gpt "
+    "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://www.metallerie.xyz",
+    "depends": ["product"],
+    "data": ["views/product_view.xml"],
+    "installable": True,
+    "images": ["static/description/icon.png"],
+}
