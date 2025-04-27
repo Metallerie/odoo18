86d20ad5e2e968094bfc4b68516fba15bb3fc48d
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-04-27 12:58:35 +0200

    Update product_metal_qty_unit.js

--- myaddons/product_metal/static/src/js/product_metal_qty_unit.js
+++ myaddons/product_metal/static/src/js/product_metal_qty_unit.js
@@ -4,36 +4,57 @@ odoo.define('product_metal.product_metal_qty_unit', function (require) {
     const publicWidget = require('web.public.widget');
 
     publicWidget.registry.ProductMetalQtyUnit = publicWidget.Widget.extend({
-        selector: '.oe_website_sale', // Le form principal
-        events: {
-            'change #unit_selector': '_onUnitChange',
-            'input input[name="add_qty"]': '_onQuantityInput',
-        },
+        selector: '.oe_website_sale',
 
         start: function () {
-            this._conversionFactor = 1; // Par défaut : mètres
-            return this._super.apply(this, arguments);
+            const self = this;
+            this._super.apply(this, arguments);
+
+            if (window.location.href.includes('metal-au-metre')) {
+                setTimeout(function () {
+                    self._injectUnitSelector();
+                }, 500);
+            }
         },
 
-        _onUnitChange: function (ev) {
-            this._conversionFactor = parseFloat(ev.currentTarget.value) || 1;
-            this._updateQuantity();
-        },
-
-        _onQuantityInput: function () {
-            this._updateQuantity();
-        },
-
-        _updateQuantity: function () {
-            const quantityInput = this.$('input[name="add_qty"]');
-            if (!quantityInput.length) {
+        _injectUnitSelector: function () {
+            const qtyInput = $('input[name="add_qty"]');
+            if (!qtyInput.length) {
+                console.log('Pas d\'input quantité trouvé.');
                 return;
             }
 
-            let rawValue = parseFloat(quantityInput.val()) || 0;
-            let convertedValue = rawValue / this._conversionFactor; // Attention : division ici pour revenir en mètre
+            // Permettre les décimales
+            qtyInput.attr('step', 'any');
 
-            quantityInput.val(convertedValue.toFixed(3));
+            // Cases à cocher
+            const unitSelector = $(`
+                <div class="input-group my-3" id="unit_selector_group">
+                    <span class="input-group-text">Unité :</span>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="unit_select" id="unit_m" value="1" checked>
+                        <label class="form-check-label" for="unit_m">Mètre (m)</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="unit_select" id="unit_cm" value="0.01">
+                        <label class="form-check-label" for="unit_cm">Centimètre (cm)</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="unit_select" id="unit_mm" value="0.001">
+                        <label class="form-check-label" for="unit_mm">Millimètre (mm)</label>
+                    </div>
+                </div>
+            `);
+
+            qtyInput.closest('.js_product_quantity').after(unitSelector);
+
+            // Gestion du changement d'unité
+            $('input[name="unit_select"]').on('change', function () {
+                const factor = parseFloat($(this).val());
+                const currentVal = parseFloat(qtyInput.val()) || 0;
+                const newVal = (currentVal * factor).toFixed(3);
+                qtyInput.val(newVal);
+            });
         },
     });
 
