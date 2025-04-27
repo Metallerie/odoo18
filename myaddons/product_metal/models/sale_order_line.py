4db44001e854e84f5470b377404b27ebc0dc9f8e
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-04-27 16:44:42 +0200

    Update sale_order_line.py

--- myaddons/product_metal/models/sale_order_line.py
+++ myaddons/product_metal/models/sale_order_line.py
@@ -4,7 +4,9 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     def _get_displayed_quantity(self):
-        """Retourne la quantité affichée avec 3 décimales."""
-        precision = 3  # On force 3 décimales
+        """Retourne la quantité affichée avec 3 décimales, ou 0.0 par défaut."""
+        if self.product_uom_qty is None:
+            return 0.0  # Retourne 0.0 si la quantité est None
+        precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
         rounded_uom_qty = round(self.product_uom_qty, precision)
         return int(rounded_uom_qty) == rounded_uom_qty and int(rounded_uom_qty) or rounded_uom_qty
