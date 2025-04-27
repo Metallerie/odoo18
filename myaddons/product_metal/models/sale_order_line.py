436b7dcc7c27475aacbadf8154d6001b69c160b3
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-04-27 19:04:04 +0200

    Update sale_order_line.py

--- myaddons/product_metal/models/sale_order_line.py
+++ myaddons/product_metal/models/sale_order_line.py
@@ -1,18 +1,9 @@
-from odoo import models
+from odoo import models, fields
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     def _get_displayed_quantity(self):
-        """Retourne la quantité affichée avec la précision définie sur la variante product.product."""
-        # Par défaut, on utilise une précision globale (par exemple, 3)
-        default_precision = 0
-
-        # Si la variante de produit a une précision personnalisée, utilisez-la
-        precision = self.product_id.uom_precision or default_precision
-
-        if self.product_uom_qty is None:
-            return 0.0  # Retourne 0.0 si la quantité est vide
-
-        rounded_uom_qty = round(self.product_uom_qty, precision)
-        return rounded_uom_qty
+        """Retourne la quantité affichée avec la précision définie sur le produit."""
+        precision = self.product_id.uom_precision or 3  # Par défaut, 3 décimales
+        return round(self.product_uom_qty, precision)
