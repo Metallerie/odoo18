cb11037d594c652ae98efe9b2f8b3a5ae21f5211
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-04-27 17:45:02 +0200

    Update product_product.py

--- myaddons/product_metal/models/product_product.py
+++ myaddons/product_metal/models/product_product.py
@@ -1,13 +1,15 @@
-# Copyright 2015 ADHOC SA  (http://www.adhoc.com.ar)
-# Copyright 2015-2016 Camptocamp SA
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
 
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
-
+    
+    #Précision personnalisée pour l'unité de mesure de cette variante de produit
+    uom_precision = fields.Integer(
+         string="Précision UoM",
+         help="Précision personnalisée pour l'unité de mesure de cette variante de produit. Laisser vide pour utiliser la précision par défaut.",
+    )
     product_length = fields.Float("length", digits=(16, 6))
     product_height = fields.Float("height", digits=(16, 6))
     product_width = fields.Float("width", digits=(16, 6))
