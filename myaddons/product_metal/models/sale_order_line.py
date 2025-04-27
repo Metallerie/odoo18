f99ee6714b1e7e42d001f7a41202acdff978f285
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-04-27 17:37:35 +0200

    Update sale_order_line.py

--- myaddons/product_metal/models/sale_order_line.py
+++ myaddons/product_metal/models/sale_order_line.py
@@ -4,12 +4,11 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     def _get_displayed_quantity(self):
-        """Retourne la quantité affichée avec la précision du produit ou la précision par défaut."""
+        """Retourne la quantité affichée avec la précision définie sur le modèle produit."""
         # Par défaut, on utilise une précision globale (par exemple, 3)
-        default_precision = 3
 
         # Si le produit a une précision personnalisée, utilisez-la
-        precision = self.product_id.uom_precision or default_precision
+        precision = self.product_id.product_tmpl_id.uom_precision or default_precision
 
         if self.product_uom_qty is None:
             return 0.0  # Retourne 0.0 si la quantité est vide
