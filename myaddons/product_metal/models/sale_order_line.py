d135e77acf4c669d1c2ce651961667b0e909ad2d
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-04-27 19:46:59 +0200

    Update sale_order_line.py

--- myaddons/product_metal/models/sale_order_line.py
+++ myaddons/product_metal/models/sale_order_line.py
@@ -1,9 +1,19 @@
-from odoo import models, fields
+import logging
+from odoo import models, fields, api
+
+_logger = logging.getLogger(__name__)
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    def _get_displayed_quantity(self):
-        """Retourne la quantité affichée avec la précision définie sur le produit."""
+    @api.onchange('product_uom_qty')
+    def _check_rounding(self):
+        """Vérifie si la quantité est arrondie et ajoute un log."""
         precision = self.product_id.uom_precision or 3  # Par défaut, 3 décimales
-        return round(self.product_uom_qty, precision)
+        rounded_qty = round(self.product_uom_qty, precision)
+        if self.product_uom_qty != rounded_qty:
+            _logger.warning(
+                "Quantité arrondie pour la ligne ID %s : de %s à %s (précision %s)",
+                self.id, self.product_uom_qty, rounded_qty, precision
+            )
+            self.product_uom_qty = rounded_qty
