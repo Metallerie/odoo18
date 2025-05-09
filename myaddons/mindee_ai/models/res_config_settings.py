fd79d9eb2d1899bae04e79d53634916b1d953790
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-05-09 08:00:12 +0200

    Update res_config_settings.py

--- myaddons/mindee_ai/models/res_config_settings.py
+++ myaddons/mindee_ai/models/res_config_settings.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import models, fields, api
 
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    mindee_api_key = fields.Char(
-        string="Clé API Mindee",
-        config_parameter='mindee_ai.api_key'
-    )
+    mindee_api_key = fields.Char(string="Clé API Mindee")
+
+    @api.model
+    def get_values(self):
+        res = super(ResConfigSettings, self).get_values()
+        res['mindee_api_key'] = self.env['ir.config_parameter'].sudo().get_param('mindee_ai.mindee_api_key', default='')
+        return res
+
+    def set_values(self):
+        super(ResConfigSettings, self).set_values()
+        self.env['ir.config_parameter'].sudo().set_param('mindee_ai.mindee_api_key', self.mindee_api_key)
