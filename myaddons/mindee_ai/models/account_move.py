e1615c7960e1b74ec413cd749e7ef5b2007c6f25
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-05-09 17:47:47 +0200

    Update account_move.py

--- myaddons/mindee_ai/models/account_move.py
+++ myaddons/mindee_ai/models/account_move.py
@@ -9,6 +9,16 @@ _logger = logging.getLogger(__name__)
 class AccountMove(models.Model):
     _inherit = 'account.move'
     pdf_attachment_id = fields.Many2one('ir.attachment', string="PDF OCR")
+    def action_open_pdf_viewer(self):
+        self.ensure_one()
+        if self.pdf_attachment_id:
+            return {
+                'type': 'ir.actions.act_url',
+                'url': f'/web/content/{self.pdf_attachment_id.id}?download=false',
+                'target': 'new',
+            }
+        return False
+        
     def action_ocr_fetch(self):
         move.pdf_attachment_id = attachment
         api_key = self.env['ir.config_parameter'].sudo().get_param('mindee_ai.mindee_api_key')
