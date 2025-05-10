22a7fdfc74706b880c86c1a2aea5182faadae52d
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-05-10 21:36:07 +0200

    Update product_product.py

--- myaddons/product_metal/models/product_product.py
+++ myaddons/product_metal/models/product_product.py
@@ -1,11 +1,9 @@
-
 from odoo import api, fields, models
-
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
     
-    #Précision personnalisée pour l'unité de mesure de cette variante de produit
+    # Précision personnalisée pour l'unité de mesure de cette variante de produit
     uom_precision = fields.Integer(
          string="Précision UoM",
          help="Précision personnalisée pour l'unité de mesure de cette variante de produit. Laisser vide pour utiliser la précision par défaut.",
@@ -28,10 +26,7 @@ class ProductProduct(models.Model):
         store=True,
     )
     
-
-    @api.depends(
-        "product_length", "product_height", "product_width", "dimensional_uom_id"
-    )
+    @api.depends("product_length", "product_height", "product_width", "dimensional_uom_id")
     def _compute_volume(self):
         template_obj = self.env["product.template"]
         for product in self:
@@ -46,3 +41,10 @@ class ProductProduct(models.Model):
     def _onchange_uom(self):
         if self.uom_id and self.uom_po_id and self.uom_id.category_id != self.uom_po_id.category_id:
             _logger.warning(f"[UOM INFO] Vente en {self.uom_id.name}, achat en {self.uom_po_id.name} pour {self.display_name}")
+
+    def _get_dimension_uom_domain(self):
+        """
+        Retourne un domaine pour filtrer les unités de mesure utilisées pour les dimensions
+        (longueur, largeur, hauteur).
+        """
+        return [("category_id.name", "=", "Length")]
