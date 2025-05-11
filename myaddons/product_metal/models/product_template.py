be2f7945b85604476bcb854468cf7a08a865e382
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-05-11 08:23:09 +0200

    Update product_template.py

--- myaddons/product_metal/models/product_template.py
+++ myaddons/product_metal/models/product_template.py
@@ -14,15 +14,7 @@ class ProductTemplate(models.Model):
         help="Précision personnalisée pour l'unité de mesure de cette variante de produit. Laisser vide pour utiliser la précision par défaut.",
     )
 
-    # Définition des champs liés dans product.template avec 'readonly=False'
-    # pour permettre de modifier les valeurs depuis product.template.
-    dimensional_uom_id = fields.Many2one(
-        "uom.uom",
-        "Dimensional UoM",
-        related="product_variant_ids.dimensional_uom_id",
-        help="Unité de mesure pour longueur, hauteur, largeur",
-        readonly=False,
-    )
+
     product_length = fields.Float(
         related="product_variant_ids.product_length",
         readonly=False,
@@ -57,45 +49,4 @@ class ProductTemplate(models.Model):
         digits=(16, 6),
     )
   
-    @api.model
-    def _calc_volume(self, product_length, product_height, product_width, uom_id):
-        volume = 0
-        if product_length and product_height and product_width and uom_id:
-            length_m = self.convert_to_meters(product_length, uom_id)
-            height_m = self.convert_to_meters(product_height, uom_id)
-            width_m = self.convert_to_meters(product_width, uom_id)
-            volume = length_m * height_m * width_m
-        return volume
-
-    @api.depends("product_length", "product_height", "product_width", "dimensional_uom_id")
-    def _compute_volume(self):
-        for template in self:
-            template.volume = template._calc_volume(
-                template.product_length,
-                template.product_height,
-                template.product_width,
-                template.dimensional_uom_id,
-            )
-
-    def convert_to_meters(self, measure, dimensional_uom):
-        uom_meters = self.env.ref("uom.product_uom_meter")
-        return dimensional_uom._compute_quantity(
-            qty=measure,
-            to_unit=uom_meters,
-            round=False,
-        )
-
-    def _prepare_variant_values(self, combination):
-        """
-        Lorsqu'une variante est créée dans la méthode create() de template,
-        et que les champs de template sont vidés après _create_variant_ids(),
-        on capture la préparation des valeurs pour les variantes ici.
-        """
-        res = super()._prepare_variant_values(combination)
-        if self.product_length:
-            res.update({"product_length": self.product_length})
-        if self.product_height:
-            res.update({"product_height": self.product_height})
-        if self.product_width:
-            res.update({"product_width": self.product_width})
-        return res
+ 
