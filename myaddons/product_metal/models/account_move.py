64bb278f9578e594f117700fe48162d09b7b1c79
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-05-12 13:46:45 +0200

    Update account_move.py

--- myaddons/product_metal/models/account_move.py
+++ myaddons/product_metal/models/account_move.py
@@ -8,14 +8,14 @@ class AccountMove(models.Model):
 
     stock_picking_id = fields.Many2one('stock.picking', string="Bon de réception lié")
 
-   def action_create_stock_picking(self):
-       StockPicking = self.env['stock.picking']
-       StockMove = self.env['stock.move']
+    def action_create_stock_picking(self):
+        StockPicking = self.env['stock.picking']
+        StockMove = self.env['stock.move']
        Location = self.env.ref('stock.stock_location_suppliers')
 
-       for move in self:
-           if move.stock_picking_id:
-               continue
+        for move in self:
+            if move.stock_picking_id:
+                continue
 
         # 🛠 Corrige les produits consu + is_storable → type = 'product'
         corrections = 0
