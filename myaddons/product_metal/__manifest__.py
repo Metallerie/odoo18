ace189c62f6950e5552cda259b783f7b7228c73f
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-05-12 21:02:36 +0200

    Update __manifest__.py

--- myaddons/product_metal/__manifest__.py
+++ myaddons/product_metal/__manifest__.py
@@ -8,11 +8,11 @@
     "depends": ['base','product','purchase','stock','website_sale','uom'],
     "data": ['views/account_move_purchase_button.xml',
         "views/product_view.xml",
-        "security/ir.model.access.csv",],
+        "security/ir.model.access.csv"],
     "installable": True,
     "images": ["static/description/icon.png"],
     'assets': {
     'web.assets_frontend': [
-        'product_metal/static/src/js/product_quantity_precision.js',
-    ]},
+        'product_metal/static/src/js/product_quantity_precision.js']
+    },
  }
