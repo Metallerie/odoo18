507f80e284f63e4153ffb7bede1d15c5fc2b3590
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-05-17 19:30:05 +0200

    Update __manifest__.py

--- myaddons/product_metal/__manifest__.py
+++ myaddons/product_metal/__manifest__.py
@@ -3,15 +3,14 @@
     'name': 'Product Métal',
     'version': '18.0.1.0.0',
     'category': 'Product',
-    'author': """Modifié par franck et Gpt Regroupement des fonctionalités de la métallerie de corneilla del vercol """,
+    'author': """Modifié par franck et Gpt Création des champ de dimention dans product_product """,
     'license': 'AGPL-3',
     'website': 'https://www.metallerie.xyz',
     'depends': [
-       "base", "purchase", "product", "stock", "website_sale", "uom",
+       "base",  "product", "uom",
     ],
     'data': [
         'security/ir.model.access.csv',
-        'views/account_move_purchase_button.xml',
         'views/product_view.xml',
     ],
     'installable': True,
