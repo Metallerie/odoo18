334582b9e62295639a553e4d3d35f82eb2876000
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-05-18 08:01:32 +0200

    Update account_move.py

--- myaddons/account_move/models/account_move.py
+++ myaddons/account_move/models/account_move.py
@@ -89,7 +89,7 @@ class AccountMove(models.Model):
                         'picking_id': picking.id,
                         'move_id': move.id,
                         'product_id': product.id,
-                        'product_uom_id': move.product_uom.id,
+                        'product_uom_id': move.product_uom_id.id,
                         'qty_done': qty,
                         'location_id': move.location_id.id,
                         'location_dest_id': move.location_dest_id.id,
