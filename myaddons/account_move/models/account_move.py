d60d465042805b5d5122cd7c37d15d4e794bdca5
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-05-18 07:54:17 +0200

    Update account_move.py

--- myaddons/account_move/models/account_move.py
+++ myaddons/account_move/models/account_move.py
@@ -85,8 +85,15 @@ class AccountMove(models.Model):
                 if po_line and product.product_kg_ml > 0 and po_line[0].product_uom.name.lower() == 'kg':
                     qty = qty / product.product_kg_ml
 
-                move_line.quantity_done = qty
-
+                    self.env['stock.move.line'].create({
+                        'picking_id': picking.id,
+                        'move_id': move.id,
+                        'product_id': product.id,
+                        'product_uom_id': move.product_uom.id,
+                        'qty_done': qty,
+                        'location_id': move.location_id.id,
+                        'location_dest_id': move.location_dest_id.id,
+                    })
             picking.button_validate()
             move.stock_picking_id = picking.id
             move.message_post(body=f"📦 Bon de réception <b>{picking.name}</b> validé à partir du bon de commande <b>{po.name}</b>.")
