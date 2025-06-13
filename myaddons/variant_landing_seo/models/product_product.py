e08fb761cde2b0efbc610052c16f946c901bb7a8
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-06-13 15:56:13 +0200

    Create product_product.py

---
+++ myaddons/variant_landing_seo/models/product_product.py
@@ -0,0 +1,19 @@
+from odoo import models, fields, api
+import re
+
+def slugify(text):
+    text = re.sub(r'\W+', '-', text.lower()).strip('-')
+    return text
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    variant_slug = fields.Char(string="Slug URL", compute="_compute_variant_slug", store=True)
+
+    @api.depends('product_tmpl_id.name', 'attribute_value_ids.name')
+    def _compute_variant_slug(self):
+        for variant in self:
+            parts = [slugify(variant.product_tmpl_id.name)]
+            values = variant.attribute_value_ids.sorted(key=lambda v: v.attribute_id.name)
+            parts += [slugify(v.name) for v in values]
+            variant.variant_slug = '-'.join(parts)
