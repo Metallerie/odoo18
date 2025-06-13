1953396cc5c00acc465e47f5153945d47644ab93
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-06-13 16:03:56 +0200

    Update product_product.py

--- myaddons/variant_landing_seo/models/product_product.py
+++ myaddons/variant_landing_seo/models/product_product.py
@@ -10,10 +10,15 @@ class ProductProduct(models.Model):
 
     variant_slug = fields.Char(string="Slug URL", compute="_compute_variant_slug", store=True)
 
-    @api.depends('product_tmpl_id.name', 'product_template_attribute_value_ids.attribute_value_id.name')
+    @api.depends('product_tmpl_id.name', 'product_template_attribute_value_ids.name')
     def _compute_variant_slug(self):
         for variant in self:
-            parts = [slugify(variant.product_tmpl_id.name)]
-            values = variant.attribute_value_ids.sorted(key=lambda v: v.attribute_id.name)
-            parts += [slugify(v.name) for v in values]
+            tmpl_name = variant.product_tmpl_id.name or ''
+            parts = [slugify(tmpl_name)]
+
+            values = variant.product_template_attribute_value_ids.sorted(
+                key=lambda val: val.attribute_id.name
+            )
+            parts += [slugify(val.name) for val in values]
+
             variant.variant_slug = '-'.join(parts)
