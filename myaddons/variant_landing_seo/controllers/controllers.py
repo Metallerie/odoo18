b038241b669e5c81509a4cd71c3c750274cd203a
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-06-13 16:22:53 +0200

    Update controllers.py

--- myaddons/variant_landing_seo/controllers/controllers.py
+++ myaddons/variant_landing_seo/controllers/controllers.py
@@ -1,22 +1,28 @@
 from odoo import http
-from odoo.http import request
+from odoo.http import request, Response
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 from odoo.tools import html_escape
 from datetime import date
-from odoo.http import Response
 
 
 class VariantLandingController(WebsiteSale):
 
-    @http.route(['/shop/<string:slug>-<int:variant_id>'], type='http', auth="public", website=True)
-    def variant_product_page(self, slug, variant_id, **kwargs):
+    @http.route(['/shop/<string:variant_slug>-<int:template_id>'], type='http', auth="public", website=True)
+    def variant_product_page(self, variant_slug, template_id, **kwargs):
+        ProductTemplate = request.env['product.template'].sudo()
         ProductProduct = request.env['product.product'].sudo()
-        variant = ProductProduct.browse(variant_id)
 
-        if not variant.exists() or not variant.website_published:
+        template = ProductTemplate.browse(template_id)
+        if not template.exists() or not template.website_published:
             return request.not_found()
 
-        template = variant.product_tmpl_id
+        variant = ProductProduct.search([
+            ('product_tmpl_id', '=', template.id),
+            ('variant_slug', '=', variant_slug)
+        ], limit=1)
+
+        if not variant:
+            return request.not_found()
 
         request.update_context(product_id=variant.id)
 
@@ -25,28 +31,33 @@ class VariantLandingController(WebsiteSale):
             'variant': variant,
         })
 
-    @http.route('/sitemap_product_variant.xml', type='http', auth='public', website=True)
-    def variant_sitemap(self):
-        products = request.env['product.product'].sudo().search([('website_published', '=', True)])
+    @http.route(['/sitemap_product_variant.xml'], type='http', auth='public', website=True, sitemap=False)
+    def variant_sitemap(self, **kwargs):
+        variants = request.env['product.product'].sudo().search([
+            ('website_published', '=', True),
+            ('variant_slug', '!=', False)
+        ])
 
-        xml_items = []
+        base_url = request.httprequest.host_url.rstrip('/')
         today = date.today().isoformat()
 
-        for product in products:
-            url = f"https://www.metallerie.xyz/shop/{product.variant_slug or product.id}"
-            xml_items.append(f"""
-    <url>
-        <loc>{url}</loc>
-        <lastmod>{today}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.8</priority>
-    </url>
-""")
+        urls = []
+        for variant in variants:
+            slug = variant.variant_slug
+            template_id = variant.product_tmpl_id.id
+            url = f"{base_url}/shop/{slug}-{template_id}"
+            urls.append(f"""
+                <url>
+                    <loc>{html_escape(url)}</loc>
+                    <lastmod>{today}</lastmod>
+                    <changefreq>weekly</changefreq>
+                    <priority>0.8</priority>
+                </url>
+            """)
 
         sitemap_content = f"""<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-{''.join(xml_items)}
-</urlset>
-"""
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            {''.join(urls)}
+        </urlset>"""
 
-        return Response(sitemap_content, content_type='application/xml;charset=utf-8')
+        return Response(sitemap_content.strip(), content_type='application/xml;charset=utf-8')
