ac78f51f57eeac9886bcae991654a87ce6a666b0
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-06-14 20:00:54 +0200

    Update product_product.py

--- myaddons/variant_landing_seo/models/product_product.py
+++ myaddons/variant_landing_seo/models/product_product.py
@@ -10,11 +10,10 @@ class ProductProduct(models.Model):
 
     variant_slug = fields.Char(string="Slug URL", compute="_compute_variant_slug", store=True)
 
-    @api.depends('product_template_attribute_value_ids.name', 'product_template_attribute_value_ids.attribute_id.name')
-    def _compute_variant_slug(self):
-        for variant in self:
-            parts = [variant.product_tmpl_id.name]  # 👈 ajoute le nom du produit principal
-            for ptav in variant.product_template_attribute_value_ids:
-                parts.append(ptav.name)
-            variant.variant_slug = slugify("-".join(parts)) if parts else False
-            product.with_context(force_write=True).write({'variant_slug': slug})
+def _compute_variant_slug(self):
+    for variant in self:
+        parts = [variant.product_tmpl_id.name]
+        for ptav in variant.product_template_attribute_value_ids:
+            parts.append(ptav.name.replace(" ", "-"))
+        slug = slugify("-".join(parts)) if parts else False
+        variant.variant_slug = slug
