740987f61ef638f4f6f3d04a501220c79a158091
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-09-05 15:50:16 +0200

    Create __manifest__.py

---
+++ myaddons/menu_menu_unlocked/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Unlock UI Menu Management",
+    "summary": "Affiche le menu Interface Utilisateur > Éléments de menu",
+    "version": "1.0",
+    "depends": ["base"],
+    "data": ["views/menu_unlock.xml"],
+    "installable": True,
+    "application": False,
+}
