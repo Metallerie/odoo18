019d67fd92dd949b8872bd7f84c26d38cf2998ee
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-10-19 20:00:09 +0200

    Update test_docai.py

--- myaddons/docai_ai/scripts/test_docai.py
+++ myaddons/docai_ai/scripts/test_docai.py
@@ -8,7 +8,7 @@ key_path = "/data/keys/docai-factures-1d0a66f84bff.json"
 client = documentai.DocumentProcessorServiceClient.from_service_account_json(key_path)
 
 # Chemin complet du processor
-name = eu-documentai.googleapis.com/v1/projects/889157590963/locations/eu/processors/a228740c1efe755d:process
+name = eu-documentai.googleapis.com/v1/projects/889157590963/locations/eu/processors/a228740c1efe755d
 # name = f"projects/{project_id}/locations/{location}/processors/{processor_id}"
 print(name)
 with open("/data/Documents/factures_archive/Facture_CCL_153880.pdf", "rb") as f:
