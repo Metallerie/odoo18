d68e2ac6063b872e9e19dc69beb7a2ec9e34ebd7
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-10-19 02:49:55 +0200

    Update test_docai.py

--- myaddons/docai_ai/scripts/test_docai.py
+++ myaddons/docai_ai/scripts/test_docai.py
@@ -1,16 +1,30 @@
-
 from google.cloud import documentai_v1 as documentai
 import os
 
-# chemin vers ta clé JSON
+# Chemin de ta clé JSON
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/data/keys/docai-factures.json"
 
-# paramètres
-project_id = "docai-factures"   # ton Project ID
-location = "eu"                 # ou "us", selon où ton processor est créé
-processor_id = "889157590963"   # ID de ton processor (une suite de chiffres)
+# Paramètres
+project_id = "889157590963"
+location = "eu"
+processor_id = "a228740c1efe755d"
+file_path = "/data/Documents/factures_archive/Facture_CCL_153880.pdf"
 
+# Client
 client = documentai.DocumentProcessorServiceClient()
 name = f"projects/{project_id}/locations/{location}/processors/{processor_id}"
 
-print("✅ Connexion réussie à Document AI :", name)
+# Lire le fichier PDF
+with open(file_path, "rb") as f:
+    pdf_content = f.read()
+
+raw_document = documentai.RawDocument(content=pdf_content, mime_type="application/pdf")
+
+# Envoyer la requête
+request = documentai.ProcessRequest(name=name, raw_document=raw_document)
+result = client.process_document(request=request)
+
+# Afficher le texte brut détecté
+document = result.document
+print("=== Texte détecté ===")
+print(document.text)
