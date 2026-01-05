cc3e70170e357b24890643cd1cbb2b9c82c9791b
Metallerie <98831730+Metallerie@users.noreply.github.com>
2026-01-05 20:20:32 +0100

    Update __manifest__.py

--- myaddons/ig_market_data/__manifest__.py
+++ myaddons/ig_market_data/__manifest__.py
@@ -1,34 +1,19 @@
-# -*- coding: utf-8 -*-
+# ig_market_data/__manifest__.py
 {
-    'name': "myaddons/ig_market_data",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
-    'description': """
-Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+    "name": "IG Market Data",
+    "version": "18.0.1.0.0",
+    "summary": "Collect IG market candles and analyze Asia opening impulse",
+    "category": "Tools",
+    "license": "LGPL-3",
+    "author": "Metallerie",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/ir_cron.xml",
+        "views/ig_instrument_views.xml",
+        "views/ig_asia_session_views.xml",
+        "views/menus.xml",
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    "installable": True,
+    "application": True,
 }
-
