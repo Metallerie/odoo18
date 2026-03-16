2f5a3c54fb0e3e305035414999145d71b1606f46
Metallerie <98831730+Metallerie@users.noreply.github.com>
2026-03-16 19:40:55 +0100

    Add SaleOrderListController for quick quote action

---
+++ myaddons/quick_quote_message/static/src/js/sale_order_list_button.js
@@ -0,0 +1,27 @@
+/** @odoo-module */
+
+import { registry } from "@web/core/registry";
+import { listView } from "@web/views/list/list_view";
+import { ListController } from "@web/views/list/list_controller";
+import { useService } from "@web/core/utils/hooks";
+
+class SaleOrderListController extends ListController {
+    setup() {
+        super.setup();
+        this.actionService = useService("action");
+    }
+
+    async onClickQuickQuote() {
+        await this.actionService.doAction("quick_quote_message.action_quick_quote_wizard");
+    }
+}
+
+SaleOrderListController.template = "quick_quote_message.SaleOrderListView.Buttons";
+
+export const saleOrderListView = {
+    ...listView,
+    Controller: SaleOrderListController,
+    buttonTemplate: "quick_quote_message.SaleOrderListView.Buttons",
+};
+
+registry.category("views").add("sale_order_quick_quote_list", saleOrderListView);
