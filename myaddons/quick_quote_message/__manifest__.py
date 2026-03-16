7d14582b93cf8c620c853bf9aafd2a31c13d8c9a
Metallerie <98831730+Metallerie@users.noreply.github.com>
2026-03-16 19:45:57 +0100

    Update __manifest__.py

--- myaddons/quick_quote_message/__manifest__.py
+++ myaddons/quick_quote_message/__manifest__.py
@@ -8,7 +8,7 @@
     "depends": ["sale_management", "product"],
     "data": [
         "views/quick_quote_wizard_views.xml",
-        "views/sale_order_quick_quote_button.xml",
+        "views/sale_order_list_inherit.xml",
     ],
     "installable": True,
     "application": False,
