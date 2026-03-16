33ffb4746d022c82cb2fd201fa1a78d6d0ed777b
Metallerie <98831730+Metallerie@users.noreply.github.com>
2026-03-16 19:57:12 +0100

    Update version and dependencies in manifest

--- myaddons/quick_quote_message/__manifest__.py
+++ myaddons/quick_quote_message/__manifest__.py
@@ -1,15 +1,20 @@
 {
     "name": "Quick Quote Message",
-    "version": "18.0.1.0.0",
+    "version": "18.0.2.0.0",
     "summary": "Devis rapide à copier-coller pour SMS ou Leboncoin",
     "category": "Sales",
     "author": "La Métallerie",
     "license": "LGPL-3",
-    "depends": ["sale_management", "product"],
+    "depends": ["sale_management", "product", "web"],
     "data": [
         "views/quick_quote_wizard_views.xml",
-        "views/sale_order_list_inherit.xml",
     ],
+    "assets": {
+        "web.assets_backend": [
+            "quick_quote_message/static/src/js/sale_order_list_button.js",
+            "quick_quote_message/static/src/xml/sale_order_list_button.xml",
+        ],
+    },
     "installable": True,
     "application": False,
 }
