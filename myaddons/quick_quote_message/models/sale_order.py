e838a1a12af89381aee42923362f5e844ec72f28
Metallerie <98831730+Metallerie@users.noreply.github.com>
2026-03-17 09:55:24 +0100

    Create sale_order.py

---
+++ myaddons/quick_quote_message/models/sale_order.py
@@ -0,0 +1,14 @@
+from odoo import models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    def action_open_quick_quote(self):
+        return {
+            "type": "ir.actions.act_window",
+            "name": "Devis rapide",
+            "res_model": "quick.quote.wizard",
+            "view_mode": "form",
+            "target": "new",
+        }
