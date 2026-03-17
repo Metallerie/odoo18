f9cd748f990752ae22eaa58358a82f4cf2d2b6af
Metallerie <98831730+Metallerie@users.noreply.github.com>
2026-03-17 14:57:47 +0100

    Update sale_order.py

--- myaddons/quick_quote_message/models/sale_order.py
+++ myaddons/quick_quote_message/models/sale_order.py
@@ -5,10 +5,14 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     def action_open_quick_quote(self):
+        self.ensure_one()
         return {
             "type": "ir.actions.act_window",
             "name": "Devis rapide",
             "res_model": "quick.quote.wizard",
             "view_mode": "form",
             "target": "new",
+            "context": {
+                "default_sale_order_id": self.id,
+            },
         }
