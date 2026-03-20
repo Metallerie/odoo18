722b98206c9f763cfcb76b946af6880762f61e76
Metallerie <98831730+Metallerie@users.noreply.github.com>
2026-03-20 16:33:12 +0100

    Update __manifest__.py

--- myaddons/product_composed_pricing/__manifest__.py
+++ myaddons/product_composed_pricing/__manifest__.py
@@ -2,10 +2,10 @@
 {
     'name': "product_composed_pricing",
 
-    'summary': "Module permettant de créer des produits composés avec calcul dynamique du prix. ",
+    'summary': "Produits composés avec calcul dynamique du prix",
 
     'description': """
-
+Module permettant de créer des produits composés avec calcul dynamique du prix.
 
 Chaque produit peut être défini à partir de plusieurs composants (matière, découpe, service, etc.).
 Une formule de quantité permet de calculer automatiquement la consommation de chaque composant
@@ -18,27 +18,17 @@ Ce module constitue une base générique pour la vente de produits sur mesure
 (ex : tôle découpée laser, pièces usinées, produits personnalisés).
 """,
 
-    'author': "My Company,chatgpt et franck ",
+    'author': "Franck / ChatGPT Pour la métallerie de coneilla-del-vercol",
     'website': "https://www.metallerie.xyz",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
+    'category': 'Sales',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
-    'depends': ['base''web','sale','website'],
+    'depends': ['base', 'product', 'sale', 'website'],
 
-    # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/product_composition_line_views.xml',
         'views/product_template_views.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
 }
-
