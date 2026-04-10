3d16b08fbbaede0e434f42467727680ebf505764
Metallerie <98831730+Metallerie@users.noreply.github.com>
2026-04-10 17:41:07 +0200

    Update account_move.py

--- myaddons/docai_ai/models/account_move.py
+++ myaddons/docai_ai/models/account_move.py
@@ -5,5 +5,5 @@ class AccountMove(models.Model):
     _inherit = "account.move"
 
     def action_docai_scan_json(self):
-    _logger.error("######## DOCAI BOUTON CLIQUÉ ########")
+        _logger.error("######## DOCAI BOUTON CLIQUÉ ########")
         raise UserError("Le bouton entre bien dans la méthode")
