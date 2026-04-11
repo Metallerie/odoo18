d4461c22ea787112fff298d0c7c6a413759b91c0
Metallerie <98831730+Metallerie@users.noreply.github.com>
2026-04-11 11:33:21 +0200

    Update __init__.py

--- myaddons/docai_ai/models/__init__.py
+++ myaddons/docai_ai/models/__init__.py
@@ -3,5 +3,6 @@
 from . import models
 from . import res_config_settings
 from . import account_move 
+from . import account_move_line
 from . import invoice_import_cron
 from . import docai_json_runner
