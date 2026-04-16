8247ab6e81668d7928cffcf4f80b1779596eac60
Metallerie <98831730+Metallerie@users.noreply.github.com>
2026-04-16 20:04:42 +0200

    Update __manifest__.py

--- myaddons/purchase_frequency_report/__manifest__.py
+++ myaddons/purchase_frequency_report/__manifest__.py
@@ -1,34 +1,19 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "purchase_frequency_report",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
-    'description': """
-Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+    "name": "Purchase Frequency Report",
+    "version": "18.0.1.0.0",
+    "summary": "Rapport de fréquence d'achat par produit",
+    "category": "Purchases",
+    "author": "La Métallerie",
+    "license": "LGPL-3",
+    "depends": [
+        "purchase",
+        "product",
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "views/purchase_frequency_views.xml",
     ],
+    "installable": True,
+    "application": False,
 }
-
