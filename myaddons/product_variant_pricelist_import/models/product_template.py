0bd09407722ed67301f2b59d9bb2e496395ca515
Metallerie <98831730+Metallerie@users.noreply.github.com>
2026-04-22 10:40:42 +0200

    Update product_template.py

--- myaddons/product_variant_pricelist_import/models/product_template.py
+++ myaddons/product_variant_pricelist_import/models/product_template.py
@@ -1,1 +1,22 @@
-│   └── product_template.py
+# product_template.py
+# -*- coding: utf-8 -*-
+
+from odoo import models
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    def action_open_variant_pricelist_import_wizard(self):
+        self.ensure_one()
+        return {
+            "type": "ir.actions.act_window",
+            "name": "Import variantes et pricelist",
+            "res_model": "product.variant.pricelist.import.wizard",
+            "view_mode": "form",
+            "target": "new",
+            "context": {
+                "default_template_id": self.id,
+                "default_category_id": self.categ_id.id,
+            },
+        }
