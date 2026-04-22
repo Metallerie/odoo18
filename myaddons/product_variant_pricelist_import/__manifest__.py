28164bfc9d1ac1ba93bc0d0d6be2577ce1a03d4a
Metallerie <98831730+Metallerie@users.noreply.github.com>
2026-04-22 10:39:23 +0200

    Update module manifest with new details and structure

--- myaddons/product_variant_pricelist_import/__manifest__.py
+++ myaddons/product_variant_pricelist_import/__manifest__.py
@@ -1,34 +1,19 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "product_variant_pricelist_import",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
-    'description': """
-Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+    "name": "Product Variant Pricelist Import",
+    "version": "18.0.1.0.0",
+    "summary": "Import de variantes et mise à jour de pricelist par CSV",
+    "category": "Sales",
+    "author": "Metallerie",
+    "license": "LGPL-3",
+    "depends": [
+        "product",
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "views/product_template_views.xml",
+        "views/product_variant_pricelist_import_wizard_views.xml",
     ],
+    "installable": True,
+    "application": False,
 }
-
