bb77f212350397a4868f6fa51ef470ec0c5d6bca
Metallerie <98831730+Metallerie@users.noreply.github.com>
2026-05-02 16:03:14 +0200

    Update manifest for To-Do Task Type module

--- myaddons/todo_task_type/__manifest__.py
+++ myaddons/todo_task_type/__manifest__.py
@@ -1,34 +1,17 @@
 # -*- coding: utf-8 -*-
+
 {
-    'name': "todo_task_type",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
-    'description': """
-Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+    "name": "To-Do Task Type",
+    "version": "18.0.1.0.0",
+    "category": "Productivity",
+    "summary": "Ajoute des types colorés aux tâches To-Do",
+    "depends": ["project_todo"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/todo_task_type_views.xml",
+        "views/project_task_views.xml",
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
 }
-
