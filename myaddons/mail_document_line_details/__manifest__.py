4c2f1abc246e565cce7219e6b6d758f99dc66d8a
Metallerie <98831730+Metallerie@users.noreply.github.com>
2026-05-11 10:58:58 +0200

    Update module manifest with new metadata

--- myaddons/mail_document_line_details/__manifest__.py
+++ myaddons/mail_document_line_details/__manifest__.py
@@ -1,34 +1,19 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "mail_document_line_details",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
-    'description': """
-Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+    "name": "Mail Document Line Details",
+    "version": "18.0.1.0.0",
+    "summary": "Ajoute le détail des lignes dans les emails de vente, achat et facturation",
+    "category": "Mail",
+    "author": "La Métallerie",
+    "license": "LGPL-3",
+    "depends": [
+        "mail",
+        "sale_management",
+        "purchase",
+        "account",
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+    "data": [
+        "data/mail_templates.xml",
     ],
+    "installable": True,
+    "application": False,
 }
-
