65d580c87a723af937bbdf384cdcc5345f65f7ed
Franck Bardina <franck@metallerie.xyz>
2026-07-16 15:24:05 +0000

    correctif

--- myaddons/docai_ai/models/__init__.py
+++ myaddons/docai_ai/models/__init__.py
@@ -2,7 +2,7 @@
 
 from . import models
 from . import res_config_settings
-from . import account_move 
+from . import account_move
 from . import account_move_line
 from . import invoice_import_cron
 from . import docai_json_runner
